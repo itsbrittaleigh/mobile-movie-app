df4765284213feeea0d934264fc9f00f46bb5767
Britta Rodenbeck <cheers@brittarodenbeck.com>
2025-08-08 15:01:13 -0500

    ✨ feat: fetching and displaying movies on home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,19 @@
 import { useRouter } from 'expo-router';
-import { Image, ScrollView, View } from "react-native";
+import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
 import { icons } from '../../constants/icons';
 import { images } from '../../constants/images';
+import { fetchMovies } from '../../services/api';
+import { useFetch } from '../../services/useFetch';
 import SearchBar from '../components/SearchBar';
 
 export default function Index() {
   const router = useRouter();
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() => fetchMovies({ query: '' }));
+
   return (
     <View className='flex-1 bg-primary'>
       <Image source={images.bg} className='absolute w-full z-0' />
@@ -18,12 +26,35 @@ export default function Index() {
         }}
       >
         <Image source={icons.logo} className='w-12 h-10 mt-20 mb-5 mx-auto' />
-        <View className='flex-1 mt-5'>
-          <SearchBar
-            onPress={() => router.push('/search')}
-            placeholder="Search for a movie"
-          />
-        </View>
+        {moviesLoading ? (
+          <ActivityIndicator size="large" color="#000FF" className='mt-10 self-center' />
+        ) : moviesError ? (
+          <Text>Error: {moviesError.message}</Text>
+        ) : (
+          <View className='flex-1 mt-5'>
+            <SearchBar
+              onPress={() => router.push('/search')}
+              placeholder="Search for a movie"
+            />
+            <Text className='text-lg text-white font-bold mt-5 mb-3'>Latest Movies</Text>
+            <FlatList
+              className='mt-2 pb-32'
+              scrollEnabled={false}
+              data={movies}
+              numColumns={3}
+              columnWrapperStyle={{
+                justifyContent: 'flex-start',
+                gap: 20,
+                paddingRight: 5,
+                marginBottom: 10,
+              }}
+              keyExtractor={(item) => item.id.toString()}
+              renderItem={({ item }) => (
+                <Text className='text-white text-sm'>{item.title}</Text>
+              )}
+            />
+          </View>
+        )}
       </ScrollView>
     </View>
   );
