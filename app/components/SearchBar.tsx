8ed71f2b73da0ab5198f301fdd13247e0ffb7436
Britta Rodenbeck <cheers@brittarodenbeck.com>
2025-08-08 14:42:49 -0500

    ✨ feat: styling home page, search bar

---
+++ app/components/SearchBar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Image, StyleSheet, TextInput, View } from 'react-native';
+import { icons } from '../../constants/icons';
+
+const SearchBar = ({ onPress, placeholder }: {
+  onPress?: () => void;
+  placeholder: string;
+}) => {
+  return (
+    <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
+      <Image source={icons.search} className='size-5' resizeMode='contain' tintColor='#AB8BF' />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value=''
+        onChangeText={() => {}}
+        placeholderTextColor='#A8B5DB'
+        className='flex-1 ml-2 text-white'
+      />
+    </View>
+  )
+}
+
+export default SearchBar
+
+const styles = StyleSheet.create({})
